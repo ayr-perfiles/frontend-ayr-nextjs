7a9a50aefaa34ee6824d8a2373da75d9c7bde77f
gsinuiri-coder <gsinuiri@gmail.com>
2026-06-23 19:55:24 -0500

    chore(config): remove debug logs from tracking and update gitignore

--- check_finishes.ts
+++
@@ -1,4 +0,0 @@
-import { listProducts } from "./src/modules/metallic-roofing/services/catalogService";
-import { db } from "./src/lib/firebase/clientApp";
-import { getDocs, collection } from "firebase/firestore";
-// Need a script to just run it, but nextjs environment might have issues with imports without tsconfig paths.
